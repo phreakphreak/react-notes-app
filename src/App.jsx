0af67ddbfbf5ad4da2f8adea46ff323693ae08ea
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-06-14 00:54:35 -0400

    refactor(Root): :zap: Add navigation and implements routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Overview, Home, Notes } from "./pages";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="flex flex-row">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/notes" element={<Notes />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
